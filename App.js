131d235e6d0116d1fb094687c0652cc45fdf8cc9
Ethan Joyce <me@ethanjoyce.xyz>
2020-05-22 23:27:56 -0230

    Add calendario and test calendar

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Calendar } from 'react-native-calendario';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Calendar
+        onChange={date => {console.log(date.startDate)}}
+        disableRange  
+      />
     </View>
   );
 }
@@ -12,8 +16,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });

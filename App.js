ffe1c5f34098dd155e90ea0e17f6ba2a05bf1ec7
Ethan Joyce <me@ethanjoyce.xyz>
2020-06-26 22:55:55 -0230

    Clean up imports

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import TrainingView from './screens/TrainingView';
@@ -16,8 +15,3 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});

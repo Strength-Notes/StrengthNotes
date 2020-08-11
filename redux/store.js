31b15eef05af57175873de18619c75ed1ff31f08
Ethan Joyce <me@ethanjoyce.xyz>
2020-08-11 18:46:36 -0230

    Remove default store data, and switch to AsyncStorage because the ExpoFilesystemStorage causes an immediate crash

--- redux/store.js
+++ redux/store.js
@@ -1,18 +1,12 @@
 import { createStore } from 'redux';
+import { AsyncStorage } from 'react-native';
 import { persistReducer, persistStore } from 'redux-persist';
-import ExpoFileSystemStorage from 'redux-persist-expo-filesystem';
 import PropTypes from 'prop-types';
-import {
-  addSetAction,
-  addExerciseAction,
-} from './actions';
 import rootReducer from './reducers';
-import { getFormattedDateString } from './organizers';
-import ExerciseProperties from './ExerciseProperties';
 
 const persistConfig = {
   key: 'root',
-  storage: ExpoFileSystemStorage,
+  storage: AsyncStorage,
   whiteList: ['sets', 'exercises'],
 };
 
@@ -21,136 +15,6 @@ const store = createStore(
 );
 
 const persistor = persistStore(store);
-
-/*
- * Default/Sample Sets
- */
-const todaysDateString = getFormattedDateString(new Date());
-
-const exampleSetSquat = {
-  key: '2020-07-02-squats-1',
-  date: todaysDateString,
-  exercise: 'Squat',
-  category: 'Lower',
-  weight: 705,
-  weightUnit: 'lbs',
-  reps: 2,
-  distance: null,
-  distanceUnit: null,
-  time: null,
-  rpe: 9,
-  comment: 'Hips shot up a little bit. Need to focus on driving through the floor.',
-};
-
-const exampleSetSquat2 = {
-  key: '2020-07-02-squats-2',
-  date: todaysDateString,
-  exercise: 'Squat',
-  category: 'Lower',
-  weight: 705,
-  weightUnit: 'lbs',
-  reps: 2,
-  distance: null,
-  distanceUnit: null,
-  time: null,
-  rpe: 9,
-  comment: 'Hips shot up a little bit. Need to focus on driving through the floor.',
-};
-
-const exampleSetBench = {
-  key: '2020-07-02-bench-1',
-  date: todaysDateString,
-  exercise: 'Bench',
-  category: 'Upper',
-  weight: 435,
-  weightUnit: 'lbs',
-  reps: 1,
-  distance: null,
-  distanceUnit: null,
-  time: null,
-  rpe: 9.5,
-  comment: 'Forgot to properly activate back.',
-};
-
-const exampleSetBench2 = {
-  key: '2020-07-02-bench-2',
-  date: todaysDateString,
-  exercise: 'Bench',
-  category: 'Upper',
-  weight: 335,
-  weightUnit: 'lbs',
-  reps: 5,
-  distance: null,
-  distanceUnit: null,
-  time: null,
-  rpe: 9,
-  comment: null,
-};
-
-function addExamples() {
-  store.dispatch(addSetAction(exampleSetSquat));
-  store.dispatch(addSetAction(exampleSetSquat2));
-  store.dispatch(addSetAction(exampleSetBench));
-  store.dispatch(addSetAction(exampleSetBench2));
-}
-
-/*
- * Default/Sample Exercises
- */
-const squatExercise = {
-  name: 'Squat',
-  primary: ExerciseProperties.WEIGHT,
-  secondary: ExerciseProperties.REPS,
-};
-
-const benchExercise = {
-  name: 'Bench',
-  primary: ExerciseProperties.WEIGHT,
-  secondary: ExerciseProperties.REPS,
-};
-
-const deadliftExercise = {
-  name: 'Deadlift',
-  primary: ExerciseProperties.WEIGHT,
-  secondary: ExerciseProperties.REPS,
-};
-
-const cleanAndJerkExercise = {
-  name: 'Clean and Jerk',
-  primary: ExerciseProperties.WEIGHT,
-  secondary: ExerciseProperties.REPS,
-};
-
-const bodyweightPullupsExercise = {
-  name: 'Bodyweight Pullups',
-  primary: ExerciseProperties.REPS,
-};
-
-const dumbbellBenchTimeExercise = {
-  name: 'Dumbbell Bench (Time)',
-  primary: ExerciseProperties.WEIGHT,
-  secondary: ExerciseProperties.TIME,
-};
-
-const farmersCarryExercise = {
-  name: 'Farmer Carry',
-  primary: ExerciseProperties.WEIGHT,
-  secondary: ExerciseProperties.DISTANCE,
-};
-
-function addExercises() {
-  store.dispatch(addExerciseAction(squatExercise));
-  store.dispatch(addExerciseAction(benchExercise));
-  store.dispatch(addExerciseAction(deadliftExercise));
-  store.dispatch(addExerciseAction(cleanAndJerkExercise));
-  store.dispatch(addExerciseAction(bodyweightPullupsExercise));
-  store.dispatch(addExerciseAction(dumbbellBenchTimeExercise));
-  store.dispatch(addExerciseAction(farmersCarryExercise));
-}
-
-addExamples();
-addExercises();
-
 export const shapeOfSetObject = PropTypes.shape({
   key: PropTypes.string.isRequired,
   date: PropTypes.string.isRequired,
